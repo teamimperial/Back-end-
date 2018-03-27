4e3f10a0a02dd478987b4a241f5c6a6bfc6aef71
DESKTOP-QUABODL\nazar <nazar.kulik99@gmail.com>
2018-03-27 22:26:04 +0300

    Delete old versions.

--- static/js/search-courses.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Nazar on 22.12.2017.
- */
-
-$('#save-button').click(function() {
-    var search = $('#search').val();
-    var redirect = '/praxis/search/' + search;
-    window.location.href = redirect;
-});
